d3dd531d0995894d6705a000b024d820b5cc88fe
Samir-1993 <samirmjd@gmail.com>
2023-11-04 17:27:51 +0530

    my portfolio website
    Hi! I'm Samir Majumdar. This is my portfolio website.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* =========== Typing animation =================== */
+
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "Graphic Designer", "UI Designer", "UX Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+/* =========== Active Class switcher =================== */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
