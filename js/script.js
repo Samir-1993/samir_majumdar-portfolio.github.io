63b8c5af9caf70c404a1d8ef4edaa03fbbb75551
Samir-1993 <samirmjd@gmail.com>
2024-03-23 16:27:59 +0530

    changes in script

--- js/script.js
+++ js/script.js
@@ -48,3 +48,23 @@ $(document).ready(function(){
 
 
 /* =========== Active class switch in menu end =================== */
+
+
+/* ===========  Prevents scroll to top when clicking on <a href="#"> start =================== */
+
+// Enable Smooth Scrolling ...
+	$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+	
+
+/* ===========  Prevents scroll to top when clicking on <a href="#"> end =================== */
